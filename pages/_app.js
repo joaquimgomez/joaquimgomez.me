7a64421b557a74986c79caf99ff9fa1409b51b97
Joaquim Gomez <9joaquimgomez@gmail.com>
2022-04-16 15:03:40 +0200

    Add new favicon

--- pages/_app.js
+++ pages/_app.js
@@ -28,6 +28,14 @@ export default function MyApp({ Component, pageProps }) {
       <Head>
         <title>Joa(quim) Gómez</title>
         <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+        <link rel="shortcut icon" type="image/png" sizes="16x16" href="/favicon/icons8-circulado-j-16.png"/>
+        <link rel="shortcut icon" type="image/png" sizes="32x32" href="/favicon/icons8-circulado-j-32.png"/>
+        <link rel="shortcut icon" type="image/png" sizes="57x57" href="/favicon/icons8-circulado-j-57.png"/>
+        <link rel="shortcut icon" type="image/png" sizes="60x60" href="/favicon/icons8-circulado-j-60.png"/>
+        <link rel="shortcut icon" type="image/png" sizes="70x70" href="/favicon/icons8-circulado-j-70.png"/>
+        <link rel="shortcut icon" type="image/png" sizes="72x72" href="/favicon/icons8-circulado-j-72.png"/>
+        <link rel="shortcut icon" type="image/png" sizes="76x76" href="/favicon/icons8-circulado-j-76.png"/>
+        <link rel="shortcut icon" type="image/png" sizes="96x96" href="/favicon/icons8-circulado-j-96.png"/>
       </Head>
       <MuiThemeProvider theme={theme}>
         <CssBaseline />
